6668f224716d4f3a3858618d8d4ca9142ef085a0
RennI <98748434+Renne103@users.noreply.github.com>
2023-06-13 23:17:44 +0300

    Add files via upload

---
+++ IDZ4/client.c
@@ -0,0 +1,76 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <time.h>
+#include <unistd.h>
+
+int generate_random_number(int min, int max) {
+    return (rand() % (max - min + 1)) + min;
+}
+
+int main(int argc, char *argv[]) {
+    if (argc < 3) {
+        printf("pls enter port and ip\n");
+        exit(1);
+    }
+
+    int port = atoi(argv[1]);
+    char *ip = argv[2];
+
+    int sock = 0, valread;
+    struct sockaddr_in serv_addr;
+    char buffer[1024] = {0};
+
+    // Create socket
+    if ((sock = socket(PF_INET, SOCK_DGRAM, IPPROTO_UDP)) < 0) {
+        printf("\nSocket creation error\n");
+        return -1;
+    }
+
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(port);
+
+    // Convert IPv4 and IPv6 addresses from text to binary form
+    if (inet_pton(AF_INET, ip, &serv_addr.sin_addr) <= 0) {
+        printf("\nInvalid address/ Address not supported\n");
+        return -1;
+    }
+
+    // Generate random budget and room type
+    srand(time(0));
+    int budget = generate_random_number(0, 10000);
+    int room_type;
+
+    // Send budget to server
+    printf("Budget: %d\n", budget);
+    snprintf(buffer, sizeof(buffer), "%d", budget);
+    sendto(sock, buffer, strlen(buffer), 0, (struct sockaddr *) &serv_addr, sizeof(serv_addr));
+
+    // Read server response
+    unsigned int addrlen_server = sizeof(serv_addr);
+    valread = recvfrom(sock, buffer, 1024, 0, (struct sockaddr *) &serv_addr, &addrlen_server);
+    printf("%s\n", buffer);
+
+    if (strcmp(buffer, "1") == 0 || strcmp(buffer, "2") == 0 || strcmp(buffer, "3") == 0) {
+        // Send room type to server
+        printf("Avaible room type: %d\n", atoi(buffer));
+        room_type = rand() % atoi(buffer) + 1;
+        printf("Chosen type: %d\n", room_type);
+        char answer[5];
+        snprintf(answer, sizeof(buffer), "%d", room_type);
+        sendto(sock, buffer, strlen(buffer), 0, (struct sockaddr *) &serv_addr, sizeof(serv_addr));
+
+        // Read server response
+        valread = recvfrom(sock, buffer, 1024, 0, (struct sockaddr *) &serv_addr, &addrlen_server);
+        printf("%s\n", buffer);
+    } else if (strcmp(buffer, "No rooms") == 0) {
+        printf("Client left the hotel due to insufficient funds.\n");
+    }
+
+    // Close socket
+    close(sock);
+
+    return 0;
+}
---
+++ IDZ4/server.c
@@ -0,0 +1,158 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <unistd.h>
+
+#define NUM_ROOMS 25
+#define MAX_CLIENTS 50
+
+typedef struct {
+    int room_num;
+    int room_type; // 1 - 2000 руб, 2 - 4000 руб, 3 - 6000 руб
+    int is_free;
+} room_t;
+
+typedef struct {
+    int num_rooms_1;
+    int num_rooms_2;
+    int num_rooms_3;
+    int num_free_rooms;
+    room_t rooms[NUM_ROOMS];
+} shared_data_t;
+
+void reserve_room(shared_data_t *shared_data, int room_type, int server_fd, struct sockaddr_in client) {
+    int i, room_num;
+    for (i = 0; i < NUM_ROOMS; i++) {
+        if (shared_data->rooms[i].room_type == room_type &&
+            shared_data->rooms[i].is_free == 1) {
+            room_num = shared_data->rooms[i].room_num;
+            shared_data->rooms[i].is_free = 0;
+            shared_data->num_free_rooms--;
+            break;
+        }
+    }
+    char response[100];
+    if (i < NUM_ROOMS) {
+        snprintf(response, sizeof(response), "Room %d reserved", room_num);
+        printf("Client reserved Room %d\n", room_num);
+    } else {
+        snprintf(response, sizeof(response), "No available rooms of type %d", room_type);
+        printf("Client requested Room of type %d, but no available rooms\n", room_type);
+    }
+    sendto(server_fd, response, strlen(response) + 1, 0, (struct sockaddr *) &client, sizeof(client));
+}
+
+int main(int argc, char *argv[]) {
+    if (argc < 2) {
+        printf("pls enter port\n");
+        exit(1);
+    }
+
+    int port = atoi(argv[1]);
+
+    int server_fd, valread;
+    struct sockaddr_in address;
+    struct sockaddr_in client;
+    int opt = 1;
+    int addrlen = sizeof(address);
+    char buffer[1024] = {0};
+    shared_data_t shared_data;
+
+    // Initialize shared data
+    shared_data.num_rooms_1 = 10;
+    shared_data.num_rooms_2 = 10;
+    shared_data.num_rooms_3 = 5;
+    shared_data.num_free_rooms = 25;
+    int i;
+    for (i = 0; i < NUM_ROOMS; i++) {
+        shared_data.rooms[i].room_num = i;
+        if (i < 10) {
+            shared_data.rooms[i].room_type = 1;
+            shared_data.rooms[i].is_free = 1;
+        } else if (i < 20) {
+            shared_data.rooms[i].room_type = 2;
+            shared_data.rooms[i].is_free = 1;
+        } else {
+            shared_data.rooms[i].room_type = 3;
+            shared_data.rooms[i].is_free = 1;
+        }
+    }
+
+    // Create socket
+    if ((server_fd = socket(PF_INET, SOCK_DGRAM, IPPROTO_UDP)) == 0) {
+        perror("socket failed");
+        exit(EXIT_FAILURE);
+    }
+
+    // Set socket options
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt))) {
+        perror("setsockopt failed");
+        exit(EXIT_FAILURE);
+    }
+
+    // Set address parameters
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(port);
+
+    // Bind socket to address
+    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
+        perror("bind failed");
+        exit(EXIT_FAILURE);
+    }
+
+    printf("Server started. Waiting for clients...\n");
+
+    // Accept client connections
+    while (1) {    
+        if (shared_data.num_free_rooms == 0) {
+            printf("No avaible rooms. Bye\n");
+            close(server_fd);
+            exit(0);
+        }
+        
+        // Read client request
+        unsigned int addrlen_client = sizeof(client);
+        valread = recvfrom(server_fd, buffer, 1024, 0, (struct sockaddr *) &client, &addrlen_client);
+
+        printf("Avaible rooms count: %d\n", shared_data.num_free_rooms);
+        printf("New client connected\n");
+
+        if (valread <= 0) {
+            printf("Client disconnected\n");
+            close(server_fd);
+            continue;
+        }
+
+        // Process client request
+        int budget = atoi(buffer);
+        printf("Client budget: %d\n", budget);
+
+        int room_type = 0;
+        if (budget >= 6000 && shared_data.num_rooms_3 > 0) {
+            room_type = 3;
+        } else if (budget >= 4000 && shared_data.num_rooms_2 > 0) {
+            room_type = 2;
+        } else if (budget >= 2000 && shared_data.num_rooms_1 > 0) {
+            room_type = 1;
+        }
+
+        if (room_type == 0) {
+            printf("Client cannot afford any available room or no rooms available\n");
+            char response[] = "No rooms";
+            sendto(server_fd, response, strlen(response) + 1, 0, (struct sockaddr *) &client, sizeof(client));
+        } else {
+            char response[5];
+            snprintf(response, sizeof(response), "%d", room_type);
+            sendto(server_fd, response, strlen(response) + 1, 0, (struct sockaddr *) &client, sizeof(client));
+            char result[5];
+            valread = recvfrom(server_fd, result, 5, 0, (struct sockaddr *) &client, &addrlen_client);
+            room_type = atoi(result);
+            reserve_room(&shared_data, room_type, server_fd, client);
+        }
+    }
+
+    return 0;
+}
